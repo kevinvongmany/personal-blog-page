529d72e4dd4af6d89a71b65f9e8169271f138eb0
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-06-30 11:47:36 +1000

    Updated JS to improve dark mode functionaly

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -3,51 +3,82 @@ const toggleIcon = document.querySelector("#toggle-icon");
 const body = document.querySelector("body");
 const moonIcon = "./assets/images/moon.png";
 const sunIcon = "./assets/images/sun.png";
-// assign a variable to a css var(--bg-color) value
 
-let mode = getMode();
 
-function updateCircleGradient(angle) {
-  let backgroundGradient = `linear-gradient(${angle}deg, var(--light-accent), var(--circle-color), var(--dark-accent))`
+const updateCircleGradient = (angle) => {
+  const backgroundGradient = `linear-gradient(${angle}deg, var(--light-accent), var(--circle-color), var(--dark-accent))`;
   const circle = document.querySelector(".circle");
-  circle.setAttribute("style", `background: ${backgroundGradient}`)
-}
+  if (!circle) return;
+  circle.setAttribute("style", `background: ${backgroundGradient}`);
+};
 
-if (mode === "dark") {
-  body.classList.add("dark");
-  updateCircleGradient(0);
-} else {
-  body.classList.add("light");
-  updateCircleGradient(180);
-}
+const setModeOnLoad = (mode) => {
+  setMode(mode);
+  if (mode === "dark") {
+    updateCircleGradient(0);
+  } else {
+    updateCircleGradient(180);
+  }
+};
 
-const toggleMode = function (event) {
-  event.preventDefault();
-  mode = getMode();
+const getMode = () => {
+  return localStorage.getItem("mode");
+};
+
+const setMode = (mode) => {
   if (mode === "light") {
     body.classList.remove("light");
     body.classList.add("dark");
     toggleIcon.setAttribute("src", moonIcon)
     updateCircleGradient(0);
-    setMode("dark");
   } else {
     body.classList.remove("dark");
     body.classList.add("light");
     toggleIcon.setAttribute("src", sunIcon)
     updateCircleGradient(180);
-    setMode("light");
   }
-};
-const toggleButton = document.querySelector("#toggle"); 
-console.log(toggleButton);
-
-toggleButton.addEventListener("click", toggleMode);
-
-// TODO: Create functions to read and write from local storage
-function getMode() {
-  return localStorage.getItem("mode");
-}
-
-function setMode(mode) {
   localStorage.setItem("mode", mode);
+};
+
+let mode = getMode();
+
+const toggleMode = function (event) {
+  event.preventDefault();
+  mode = getMode();
+  setMode(mode === "light" ? "dark" : "light");
+  // if (mode === "light") {
+  //   body.classList.remove("light");
+  //   body.classList.add("dark");
+  //   toggleIcon.setAttribute("src", moonIcon)
+  //   updateCircleGradient(0);
+  //   setMode("dark");
+  // } else {
+  //   body.classList.remove("dark");
+  //   body.classList.add("light");
+  //   toggleIcon.setAttribute("src", sunIcon)
+  //   updateCircleGradient(180);
+  //   setMode("light");
+  // }
+};
+
+const init = () => {
+  // Ensures the transition only applies on toggle and not on page load
+  document.addEventListener('DOMContentLoaded', () => {
+    // Temporarily disable transitions
+    document.body.classList.add('no-transition');
+
+    // Check if dark mode is enabled and apply the dark class if necessary
+    setModeOnLoad(getMode());
+
+    // Re-enable transitions after a short delay
+    setTimeout(() => {
+      document.body.classList.remove('no-transition');
+    }, 50);
+  });
+  const toggleButton = document.querySelector("#toggle"); 
+  console.log(toggleButton);
+
+  toggleButton.addEventListener("click", toggleMode);
 }
+
+init();
